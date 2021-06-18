d79fcbb401a87ad8709acfbae5f720c860f69224
mvillarreal@saludcastillayleon.es <mvillarreal@saludcastillayleon.es>
2021-06-18 20:55:28 +0200

    Ejercicio 21 y 22

--- src/app/datos-bancarios/datos-bancarios.component.ts
+++ src/app/datos-bancarios/datos-bancarios.component.ts
@@ -7,21 +7,21 @@ import { NgForm } from '@angular/forms';
   <form (ngSubmit)="enviar(f)" #f="ngForm">
     <div>
       <label>Entidad&emsp;</label>
-      <input name="entidad" ngModel>
+      <input name="entidad" ngModel required minlength="4" maxlength="4" size="4">
     </div>
     <div>
       <label>Sucursal&emsp;</label>
-      <input name="sucursal" ngModel>
+      <input name="sucursal" ngModel required minlength="4" maxlength="4" size="4">
     </div>
     <div>
       <label>DC&emsp;</label>
-      <input name="dc" ngModel>
+      <input name="dc" ngModel required minlength="2" maxlength="2" size="2">
     </div>
     <div>
       <label>Cuenta&emsp;</label>
-      <input name="cuenta" ngModel>
+      <input name="cuenta" ngModel required minlength="10" maxlength="10" size="10">
     </div>
-    <button>Enviar</button>
+    <button [disabled]="f.invalid">Enviar</button>
   </form>
   `,
   styleUrls: ['./datos-bancarios.component.css']
@@ -35,6 +35,18 @@ export class DatosBancariosComponent implements OnInit {
 
   enviar(f: NgForm) {
     console.log(f);
+    if (f.controls.entidad.errors) {
+      console.log("Error en la entidad: " + f.controls.entidad.errors);
+    }
+    if (f.controls.sucursal.errors) {
+      console.log("Error en la sucursal: " + f.controls.sucursal.errors);
+    }
+    if (f.controls.dc.errors) {
+      console.log("Error en el dígito de control: " + f.controls.dc.errors);
+    }
+    if (f.controls.cuenta.errors) {
+      console.log("Error en el nº de cuenta: " + f.controls.cuenta.errors);
+    }
   }
 
 }
